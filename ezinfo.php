aa2636364db9d128a46eb67dc5fd151c85382739
Paul Forsyth <info@paulforsyth.com>
2007-11-05 23:53:35 +0000

    Add ezinfo file
    git-svn-id: http://svn.projects.ez.no/opensocial@1 84ee4c64-5a98-4870-b1b5-a9336c939155

---
+++ ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class opensocialInfo
+{
+    function info()
+    {
+        return array( 'name' => "OpenSocial stuff",
+                      'version' => "0.0000000000001",
+                      'copyright' => "Copyright (C) 2007 Paul Forsyth",
+                      'license' => "GNU General Public License v2.0"
+                    );
+    }
+}
+?>
